ca819e2cf16600a277f509393cde9f50edddb1bd
jcs <jiangchangshun@cloudunion.cn>
2014-08-22 14:14:17 +0800

    lib huobi,okcoin finish

--- lib/Huobi.php
+++ lib/Huobi.php
@@ -1,7 +1,5 @@
 <?php
 class Huobi{
-    const ACCESS_KEY = '';
-    const SECRET_KEY = '';
     const API_URL = 'https://api.huobi.com/api.php';
 
     public static function httpRequest($param, $pUrl='https://api.huobi.com/api.php'){
@@ -21,7 +19,7 @@ class Huobi{
     }
 
     public static function createSign($params = array()){
-        $params['secret_key'] = self::SECRET_KEY;
+        $params['secret_key'] = SECRET_HUOBI;
         ksort($params);
         $preSign = http_build_query($params);
         $sign = md5($preSign);
@@ -44,7 +42,37 @@ class Huobi{
     public static function getAccountInfo(){
         $params = array();
         $params['method'] = 'get_account_info';
-        $params['access_key'] = self::ACCESS_KEY;
+        $params['access_key'] = KEY_HUOBI;
+        $params['created'] = time();
+        $params['sign'] = self::createSign($params);
+        return self::httpRequest($params);
+    }
+    /**
+     * 限价单买入
+     */
+    public static function buy($price, $amount)
+    {
+        $params = array();
+        $params['method'] = 'buy';
+        $params['access_key'] = KEY_HUOBI;
+        $params['coin_type'] = 1;
+        $params['price'] = $price;
+        $params['amount'] = $amount;
+        $params['created'] = time();
+        $params['sign'] = self::createSign($params);
+        return self::httpRequest($params);
+    }
+    /**
+     * 限价单卖出
+     */
+    public static function sell($price, $amount)
+    {
+        $params = array();
+        $params['method'] = 'sell';
+        $params['access_key'] = KEY_HUOBI;
+        $params['coin_type'] = 1;
+        $params['price'] = $price;
+        $params['amount'] = $amount;
         $params['created'] = time();
         $params['sign'] = self::createSign($params);
         return self::httpRequest($params);
--- lib/Okcoin.php
+++ lib/Okcoin.php
@@ -1,20 +1,18 @@
 <?php
 class Okcoin {
-    const  PARTNER = "";
-    const  SECRETKEY ="";
 
-    protected static function okQuery($parameters, $url){
-        $post_data = http_build_query($parameters, '', '&');
-        $sign = md5 ($post_data.self::SECRETKEY);
+    protected static function okQuery($params, $url){
+        $post_data = http_build_query($params, '', '&');
+        $sign = md5 ($post_data.KEY_OKCOIN);
         $sign = strtoupper($sign);
 
-        $query = "partner=".self::PARTNER."&sign=".$sign."&".$post_data;
+        $query = "partner=".KEY_OKCOIN."&sign=".$sign."&".$post_data;
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $res = curl_exec($ch);
         $res = json_decode ($res,true);
         return $res;
@@ -36,44 +34,54 @@ class Okcoin {
         return $ans;
     }
 
-    public static function trade($Price,$Amount,$Direction){
-        $parameters=array(
-            "amount" => $Amount,
-            "partner" => self::PARTNER,
-            "rate" => $Price,
+    /**
+     * 下单
+     * @param $price 买入/或卖出价格
+     * @param $amount 数量
+     * @parma $type 市价单/限价单
+     *
+     */
+    public static function trade($price,$amount,$type){
+        $params=array(
+            "amount" => $amount,
+            "partner" => KEY_OKCOIN,
+            "rate" => $price,
             "symbol" => 'btc_cny',
-            "type" => strtolower($Direction)
+            "type" => strtolower($type)
         );
         $url = 'https://www.okcoin.com/api/trade.do';
-        $res = self::okQuery($parameters, $url);
+        $res = self::okQuery($params, $url);
         return $res;
     }       
 
-    public static function cancelOrder($OrderID){
-        $parameters=array("order_id"=>$OrderID,"partner"=>self::PARTNER,"symbol"=>"btc_cny");
+    public static function cancelOrder($id){
+        $params = array("order_id"=>$id, "partner"=>KEY_OKCOIN, "symbol"=>"btc_cny");
         $url = 'https://www.okcoin.com/api/cancelorder.do';
-        $res = self::okQuery($parameters, $url);
+        $res = self::okQuery($params, $url);
         return $res;
     }
 
-    public static function fund(){
-        $parameters = array("partner"=>self::PARTNER);
+    public static function getUserInfo(){
+        $params = array("partner"=>KEY_OKCOIN);
         $url = 'https://www.okcoin.com/api/userinfo.do';
-        $res = self::okQuery($parameters, $url);
+        $res = self::okQuery($params, $url);
 
         if($res["result"]){
-            $res = array("result"=>true, "Frozen"=>array("BTC"=>$res["info"]["funds"]["freezed"]["btc"], "CNY"=>$res["info"]["funds"]["freezed"]["cny"]),
-            "Free"=>array("BTC"=>$res["info"]["funds"]["free"]["btc"], "CNY"=>$res["info"]["funds"]["free"]["cny"])); 
-            return $res;
+            $res = array(
+                "result"=>true, 
+                "frozen"=>array("btc"=>$res["info"]["funds"]["freezed"]["btc"], "cny"=>$res["info"]["funds"]["freezed"]["cny"]),
+                "free"=>array("btc"=>$res["info"]["funds"]["free"]["btc"], "cny"=>$res["info"]["funds"]["free"]["cny"])
+            ); 
+        } else {
+            $res = array("result"=>false);
         }
-        $res = array("result"=>false);
         return $res;
     } 
 
-    public static function getOrder($orderID){
-        $parameters = array("order_id"=>$orderID,"partner"=>self::PARTNER,"symbol"=>"btc_cny");
+    public static function getOrder($id){
+        $params = array("order_id"=>$id,"partner"=>KEY_OKCOIN,"symbol"=>"btc_cny");
         $url = 'https://www.okcoin.com/api/getorder.do';
-        $res = self::okQuery($parameters, $url);
+        $res = self::okQuery($params, $url);
         return $res;
     }
 }
