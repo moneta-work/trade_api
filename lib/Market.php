60adf14ff4eee92cd4892eb65d7958f878141043
jcs <jiangchangshun@cloudunion.cn>
2014-08-22 14:13:36 +0800

    add btcc market

--- lib/Market.php
+++ lib/Market.php
@@ -5,13 +5,16 @@ class Market{
     #
     const OKCOIN_TICKER = 'https://www.okcoin.cn/api/ticker.do';
     const OKCOIN_DEPTH = 'https://www.okcoin.cn/api/depth.do';
+    //
+    const BTCC_TICKER = 'https://data.btcchina.com/data/ticker?market=';
+    const BTCC_DEPTH = '';
     #
     const BTER_TICKER = 'http://data.bter.com/api/1/ticker/';
     const BTER_DEPTH = 'http://data.bter.com/api/1/depth/';
     public static function httpRequest($url){
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-type: application/x-www-form-urlencoded"));
-        curl_setopt($ch, CURLOPT_URL, $pUrl);
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec($ch);
@@ -21,14 +24,14 @@ class Market{
     /**
      * 火币实时行情
      */
-    public static function huobiTicker($coin){
+    public static function huobiTicker($coin='btc'){
         $data_url = self::HUOBI_MARKET."ticker_{$coin}_json.js";
         return self::httpRequest($data_url);
     }
     /**
      * 火币市场深度
      */
-    public static function huobiDepth($coin){
+    public static function huobiDepth($coin='btc'){
         $data_url = self::HUOBI_MARKET."depth_{$coin}_json.js";
         return self::httpRequest($data_url);
     }
@@ -50,6 +53,13 @@ class Market{
         if(!empty($coin) && $coin == 'ltc'){
             $data_url = self::OKCOIN_DEPTH."?symbol=ltc_cny";
         }
+        return self::httpRequest($data_url);
+    }
+    /**
+     * 比特币中国交易行情
+     */
+    public static function btccTicker($market='btccny'){
+        $data_url = self::BTCC_TICKER.$market;
         return self::httpRequest($data_url);
     }
     /**
